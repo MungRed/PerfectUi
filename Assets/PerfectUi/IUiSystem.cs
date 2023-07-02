c744653b96bbcba81121c19bd53eda9e05ae0862
MungRed <MungRed@163.com>
2023-07-02 15:34:13 +0800

    项目创建

--- Assets/PerfectUi/IUiSystem.cs
+++ Assets/PerfectUi/IUiSystem.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IUiSystem : MonoBehaviour
+namespace PerfectUi
 {
-    // Start is called before the first frame update
-    void Start()
+    public interface IUiSystem
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
+
